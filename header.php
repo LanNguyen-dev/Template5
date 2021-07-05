0bf5e065a581eae8cb70f630b7bbd0e493967ebd
LanNguyen-dev <nguyenlanhn.dev@gmail.com>
2021-07-06 06:44:33 +0700

    add header.php

---
+++ header.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ant Home</title>
+    <link rel="shortcut icon" href="img/logo.png"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/font-awesome.min.css" rel="stylesheet"/>
+    <link href="css/slick.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"/>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <header>
+        <div class="header_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2 col-md-3 d-block d-lg-none">
+                        <div class="nav-toggle">
+                            <i class="fa fa-align-left" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-7 col-7">
+                        <div class="logo">
+                            <a href="">
+                                <img src="img/logo.png" alt="" class="mw-100">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-1 col-1">
+                        <div class="icon_search d-block d-lg-none">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </div>
+                        <div class="search_pc">
+                            <p class="d-block d-lg-none"><b>TÌM KIẾM SẢN PHẨM</b></p>
+                            <svg class="icon_close d-block d-lg-none" viewBox="0 0 16 14"> 
+                                <path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none" fill-rule="evenodd"></path> 
+                            </svg>
+                            <form action="" class="position-relative">
+                                <input type="text" class="form-control" placeholder="Tìm sản phẩm ...">
+                                <button class="btn_search position-absolute">
+                                    <svg class="Icon Icon--search-desktop" viewBox="0 0 21 21">
+                                        <g transform="translate(1 1)" stroke="currentColor" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="square">
+                                            <path d="M18 18l-5.7096-5.7096"></path>
+                                            <circle cx="7.2" cy="7.2" r="7.2"></circle>
+                                        </g>
+                                    </svg>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-1 col-2">
+                        <div class="group_header_right d-flex">
+                            <a href="" class="cl_0 position-relative d-none d-lg-block" title="Sản phẩm yêu thích">
+                                <i class="fa fa-heart-o" aria-hidden="true"></i> Yêu thích
+                                <span class="count_right position-absolute">0</span>
+                            </a>
+                            <a href="" class="cl_0 position-relative d-none d-lg-block" title="Sản phẩm yêu thích">
+                                <i class="fa fa-user-o" aria-hidden="true"></i> Tài khoản
+                            </a>
+                            <a class="cart_top cl_0 position-relative" title="Sản phẩm yêu thích">
+                                <i class="fa fa-shopping-basket" aria-hidden="true"></i> <span class="d-none d-lg-block">Giỏ hàng</span>
+                                <span class="count_right position-absolute">0</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="header_menu">
+            <div class="container">
+                <div class="nav-container">
+                    <svg class="icon_close d-block d-lg-none" viewBox="0 0 16 14"> 
+                        <path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none" fill-rule="evenodd"></path> 
+                    </svg>
+                    <ul class="nav-menu menu clearfix list-inline">
+                        <li class="menu-item">
+                            <a href="" class="menu-link cl_0">Trang chủ</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="" class="menu-link cl_0">Giới thiệu</a>
+                        </li>
+                        <li class="menu-item has-dropdown">
+                            <a href="" class="menu-link cl_0">Sản phẩm</a>
+                            <div class="nav-dropdown">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-lg-8">
+                                            <ul class="menu_child list-inline">
+                                                <li class="menu-item has-dropdown">
+                                                    <a href="" class="menu-link cl_0">Nội thất</a>
+                                                    <ul class="nav-dropdown list-inline">
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng khách</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng ăn</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng ngủ</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng làm việc</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng cho bé</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li class="menu-item has-dropdown">
+                                                    <a href="" class="menu-link cl_0">Đồ trang trí</a>
+                                                    <ul class="nav-dropdown list-inline">
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng khách</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng ăn</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng ngủ</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng làm việc</a>
+                                                        </li>
+                                                        <li class="menu-item">
+                                                            <a href="" class="menu-link cl_0">Phòng cho bé</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a href="" class="menu-link cl_0">Đèn</a>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a href="" class="menu-link cl_0">Ngoại thất</a>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a href="" class="menu-link cl_0">Đồ dùng nhà tắm</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-lg-4 d-none d-lg-block">
+                                            <a href=""><img src="img/qc_menu.jpg" alt="" class="w-100"></a>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item"><a href="" class="menu-link cl_0">Deal hot</a></li>
+                        <li class="menu-item"><a href="" class="menu-link cl_0">Thương hiệu nổi bật</a></li>
+                        <li class="menu-item"><a href="" class="menu-link cl_0">Tin tức</a></li>
+                        <li class="menu-item"><a href="" class="menu-link cl_0">Liên hệ</a></li>
+                        <li class="menu-item d-block d-lg-none">
+                            <a href="" class="menu-link cl_0">Đăng nhập</a>
+                        </li>
+                        <li class="menu-item d-block d-lg-none">
+                            <a href="" class="menu-link cl_0">Đăng ký</a>
+                        </li>
+                        <li class="menu-item d-block d-lg-none">
+                            <a href="" class="menu-link cl_0">Sản phẩm yêu thích (0)</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <div class="opacity_menu"></div>
+        <div class="show_cart position-fixed">
+            <div class="title_show_cart"><b>GIỎ HÀNG</b></div>
+            <div class="close_show">
+                <svg class="icon_close" viewBox="0 0 16 14"> 
+                    <path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none" fill-rule="evenodd"></path> 
+                </svg>
+            </div>
+            <div class="list_product_addcart">
+                <div class="product_addcart_item position-relative">
+                    <a href="" class="img_product_addcart">
+                        <img src="img/prd1.jpg" alt="">
+                    </a>
+                    <div class="product_addcart_content">
+                        <a href="" class="product_name cl_0">Bộ 2 Bàn Sofa IGEA Việt Nam Phong Cách Scanvadian Màu Trắng</a>
+                        <div class="group_box d-flex">
+                            <div class="box_number">
+                                <small>Số lượng</small>
+                                <div class="number_product_cart">
+                                    <div class="d-flex">
+                                        <div class="value-button decreaseValue" onclick="decreaseValue('product1')">-</div>
+                                        <input class="input-value-button" size="1" id="product1" name="product1" value="1" min="1" max="99" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');"/>
+                                        <div class="value-button increaseValue" onclick="increaseValue('product1')">+</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="box_price_right">
+                                <div class="price_sell">4.200.000₫</div>
+                                <a class="delete_product cl_0">Bỏ sản phẩm</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_addcart_item position-relative">
+                    <a href="" class="img_product_addcart">
+                        <img src="img/prd2.jpg" alt="">
+                    </a>
+                    <div class="product_addcart_content">
+                        <a href="" class="product_name cl_0">Bộ 2 Bàn Sofa IGEA Việt Nam Phong Cách Scanvadian Màu TrắngR</a>
+                        <div class="group_box d-flex">
+                            <div class="box_number">
+                                <small>Số lượng</small>
+                                <div class="number_product_cart">
+                                    <div class="d-flex">
+                                        <div class="value-button decreaseValue" onclick="decreaseValue('product2')">-</div>
+                                        <input class="input-value-button" size="1" id="product2" name="product1" value="1" min="1" max="99" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');"/>
+                                        <div class="value-button increaseValue" onclick="increaseValue('product2')">+</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="box_price_right">
+                                <div class="price_sell">4.200.000₫</div>
+                                <a class="delete_product cl_0">Bỏ sản phẩm</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <div class="sum_all position-absolute">
+                <div class="sum_money d-flex">
+                    <span>Tổng tiền:</span>
+                    <span class="sum">4.200.000₫</span>
+                </div>
+                <a href="" class="d-block w-100 text-center pay_now">Thanh toán</a>
+            </div>  
+        </div>
+    </header>
